02bd3ccbdd0ef170551e1100b98223c738bebb6d
tgillette <timothygillette@ymail.com>
2017-07-31 16:43:58 -0400

    Base Database tables for testing on users.

--- Movie Theater.sql
+++ Movie Theater.sql
@@ -13,4 +13,53 @@ grant create session to theater;
 grant create table to theater;
 grant create view to theater;
 
+create table theater.Accounts(
+userId number primary key,
+uname varchar2(50) unique,
+pword varchar2(50),
+email varchar2(50),
+fname varchar2(50),
+lname varchar2(50)
+);
+
+create table theater.Movies(
+movieId number primary key,
+mtitle varchar2(50),
+releasedate timestamp,
+mgenre varchar(50),
+mlength number(3,0)
+);
+
+create table theater.Halls(
+hallId number primary key,
+hcapacity number,
+hcost number(4, 2)
+);
+
+create table theater.Showtimes(
+showId number primary key,
+showtimes timestamp unique
+);
+
+create table theater.MovieInfo(
+infoId number primary key
+movieId number,
+hallId number,
+showId number,
+onlineTot number,
+walkTot number
+);
+
+create table theater.Transaction(
+transId number primary key,
+userId number,
+infoId number
+);
+
+create table theater.Employee(
+infoId number,
+walkAmount number,
+userId number
+);
+
 commit;
